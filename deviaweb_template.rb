bcfc18438261c761bc34b1c5dc6f2d0e25747571
eric pinto <eric.pinto@hotmail.fr>
2022-10-10 17:39:39 +0200

    add layouts

--- deviaweb_template.rb
+++ deviaweb_template.rb
@@ -38,11 +38,102 @@ end
 # Layout
 ########################################
 
-gsub_file(
-  "app/views/layouts/application.html.erb",
-  '<meta name="viewport" content="width=device-width,initial-scale=1">',
-  '<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">'
-)
+# gsub_file(
+#   "app/views/layouts/application.html.erb",
+#   '<meta name="viewport" content="width=device-width,initial-scale=1">',
+#   '<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">'
+# )
+
+# application.html.erb
+########################################
+run "rm -rf app/views/layouts/application.html.erb"
+file "app/views/layouts/application.html.erb", <<~HTML
+<!DOCTYPE html>
+<html>
+  <head>
+    <!-- titre et description -->
+    <title>Deviaweb</title>
+    <meta name="description" content="Développeurs intelligence artificielle et applications Web">
+
+    <!-- responsive -->
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <%= csrf_meta_tags %>
+    <%= csp_meta_tag %>
+
+    <!-- CSS Javascript -->
+    <%= stylesheet_link_tag "application", media: 'all', "data-turbo-track": "reload" %>
+    <%= javascript_include_tag "application", "data-turbo-track": "reload", defer: true %>
+
+    <!-- favicon -->
+    <%= favicon_link_tag asset_path('https://raw.githubusercontent.com/ouic/fullstack-images/master/uikit/deviaweb_logo_et_fond.png') %>
+
+    <!-- compatibilité microsoft Edge -->
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+
+    <!-- bootstrap -->
+    <%# <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css"> %>
+
+    <!-- fontawesome -->
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.10/css/all.css">
+  </head>
+
+  <body>
+      <%= render "shared/navbar" %>
+      <%= render "shared/flashes" %>
+
+      <%= yield %>
+
+      <!-- Including Bootstrap JS (with its Popper.js / jQuery dependency) so that dynamic components work -->
+      <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+      <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
+      <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+
+  </body>
+</html>
+HTML
+
+# navbar.html.erb
+########################################
+run "curl -L https://raw.githubusercontent.com/lewagon/awesome-navbars/master/templates/_navbar_wagon.html.erb > app/views/shared/_navbar.html.erb"
+run "rm -rf app/views/shared/_navbar.html.erb"
+file "app/views/shared/_navbar.html.erb", <<~HTML
+<div class="navbar navbar-expand-sm navbar-light navbar-lewagon">
+  <div class="container-fluid">
+    <div class="logo">
+      <%= link_to root_path, class: "navbar-brand" do %>
+        <%= image_tag "logo.png", class: "logo" %>
+        <h1>Dev<br>ia<br>web</h1>
+      <% end %>
+    </div>
+
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto">
+        <% if user_signed_in? %>
+          <li class="nav-item active">
+            <%= link_to "Menu", menu_path, class: "btn-navbar" %>
+          </li>
+          <li class="nav-item dropdown">
+            <%= image_tag "profile_picture.png", class: "avatar-bordered dropdown-toggle", id: "navbarDropdown", data: { bs_toggle: "dropdown" }, 'aria-haspopup': true, 'aria-expanded': false %>
+            <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
+              <%= link_to "Menu", menu_path, class: "dropdown-item" %>
+              <%= link_to "Déconnexion", destroy_user_session_path, data: {turbo_method: :delete}, class: "dropdown-item" %>
+            </div>
+          </li>
+        <% else %>
+          <li class="nav-item">
+            <%= link_to "S'inscrire", new_user_registration_path, class: "btn-navbar" %>
+            <%= link_to "Connexion", new_user_session_path, class: "btn-navbar" %>
+          </li>
+        <% end %>
+      </ul>
+    </div>
+  </div>
+</div>
+HTML
 
 # Flashes
 ########################################
@@ -63,13 +154,27 @@ file "app/views/shared/_flashes.html.erb", <<~HTML
   <% end %>
 HTML
 
-run "curl -L https://raw.githubusercontent.com/lewagon/awesome-navbars/master/templates/_navbar_wagon.html.erb > app/views/shared/_navbar.html.erb"
+# inject_into_file "app/views/layouts/application.html.erb", after: "<body>" do
+#   <<~HTML
+#     <%= render "shared/navbar" %>
+#     <%= render "shared/flashes" %>
+#   HTML
+# end
 
-inject_into_file "app/views/layouts/application.html.erb", after: "<body>" do
-  <<~HTML
-    <%= render "shared/navbar" %>
-    <%= render "shared/flashes" %>
-  HTML
+# package.json
+########################################
+gsub_file(
+  "package.json",
+  '"name": "app",',
+  '"name": "deviaweb.fr",'
+)
+inject_into_file "package.json", after: '"webpack-cli": "^4.10.0"' do
+  <<~TEXT
+  },
+  "scripts": {
+    "build": "webpack --config webpack.config.js"
+  }
+  TEXT
 end
 
 # README
@@ -130,6 +235,11 @@ after_bundle do
   end
 
   generate("scaffold_controller", "User", "email", "nickname", "first_name", "last_name", "phone_number", "birth_date:date", "gender", "ip_address", "admin:boolean")
+  gsub_file(
+    "config/routes.rb",
+    '  resources :users',
+    ''
+  )
 
   # config navigational_formats
   inject_into_file "config/initializers/devise.rb", after: "# config.navigational_formats = ['*/*', :html]" do
