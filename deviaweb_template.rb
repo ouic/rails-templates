7a0f13b2c20dded7b38fef4c130cb274ea97d45a
eric pinto <eric.pinto@hotmail.fr>
2022-10-10 18:13:08 +0200

    add menu page

--- deviaweb_template.rb
+++ deviaweb_template.rb
@@ -48,48 +48,48 @@ end
 ########################################
 run "rm -rf app/views/layouts/application.html.erb"
 file "app/views/layouts/application.html.erb", <<~HTML
-<!DOCTYPE html>
-<html>
-  <head>
-    <!-- titre et description -->
-    <title>Deviaweb</title>
-    <meta name="description" content="Développeurs intelligence artificielle et applications Web">
+  <!DOCTYPE html>
+  <html>
+    <head>
+      <!-- titre et description -->
+      <title>Deviaweb</title>
+      <meta name="description" content="Développeurs intelligence artificielle et applications Web">
 
-    <!-- responsive -->
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <%= csrf_meta_tags %>
-    <%= csp_meta_tag %>
+      <!-- responsive -->
+      <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+      <%= csrf_meta_tags %>
+      <%= csp_meta_tag %>
 
-    <!-- CSS Javascript -->
-    <%= stylesheet_link_tag "application", media: 'all', "data-turbo-track": "reload" %>
-    <%= javascript_include_tag "application", "data-turbo-track": "reload", defer: true %>
+      <!-- CSS Javascript -->
+      <%= stylesheet_link_tag "application", media: 'all', "data-turbo-track": "reload" %>
+      <%= javascript_include_tag "application", "data-turbo-track": "reload", defer: true %>
 
-    <!-- favicon -->
-    <%= favicon_link_tag asset_path('https://raw.githubusercontent.com/ouic/fullstack-images/master/uikit/deviaweb_logo_et_fond.png') %>
+      <!-- favicon -->
+      <%= favicon_link_tag asset_path('https://raw.githubusercontent.com/ouic/fullstack-images/master/uikit/deviaweb_logo_et_fond.png') %>
 
-    <!-- compatibilité microsoft Edge -->
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+      <!-- compatibilité microsoft Edge -->
+      <meta http-equiv="X-UA-Compatible" content="IE=edge">
 
-    <!-- bootstrap -->
-    <%# <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css"> %>
+      <!-- bootstrap -->
+      <%# <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css"> %>
 
-    <!-- fontawesome -->
-    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.10/css/all.css">
-  </head>
+      <!-- fontawesome -->
+      <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.10/css/all.css">
+    </head>
 
-  <body>
-      <%= render "shared/navbar" %>
-      <%= render "shared/flashes" %>
+    <body>
+        <%= render "shared/navbar" %>
+        <%= render "shared/flashes" %>
 
-      <%= yield %>
+        <%= yield %>
 
-      <!-- Including Bootstrap JS (with its Popper.js / jQuery dependency) so that dynamic components work -->
-      <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
-      <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
-      <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+        <!-- Including Bootstrap JS (with its Popper.js / jQuery dependency) so that dynamic components work -->
+        <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+        <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
 
-  </body>
-</html>
+    </body>
+  </html>
 HTML
 
 # Flashes
@@ -125,43 +125,91 @@ run "curl -L https://raw.githubusercontent.com/lewagon/awesome-navbars/master/te
 run "rm -rf app/views/shared/_navbar.html.erb"
 
 file "app/views/shared/_navbar.html.erb", <<~HTML
-<div class="navbar navbar-expand-sm navbar-light navbar-lewagon">
-  <div class="container-fluid">
-    <div class="logo">
-      <%= link_to root_path, class: "navbar-brand" do %>
-        <%= image_tag "logo.png", class: "logo" %>
-        <h1>Dev<br>ia<br>web</h1>
-      <% end %>
-    </div>
-
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul class="navbar-nav me-auto">
-        <% if user_signed_in? %>
-          <li class="nav-item active">
-            <%= link_to "Menu", menu_path, class: "btn-navbar" %>
-          </li>
-          <li class="nav-item dropdown">
-            <%= image_tag "profile_picture.png", class: "avatar-bordered dropdown-toggle", id: "navbarDropdown", data: { bs_toggle: "dropdown" }, 'aria-haspopup': true, 'aria-expanded': false %>
-            <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
-              <%= link_to "Menu", menu_path, class: "dropdown-item" %>
-              <%= link_to "Déconnexion", destroy_user_session_path, data: {turbo_method: :delete}, class: "dropdown-item" %>
-            </div>
-          </li>
-        <% else %>
-          <li class="nav-item">
-            <%= link_to "S'inscrire", new_user_registration_path, class: "btn-navbar" %>
-            <%= link_to "Connexion", new_user_session_path, class: "btn-navbar" %>
-          </li>
+  <div class="navbar navbar-expand-sm navbar-light navbar-lewagon">
+    <div class="container-fluid">
+      <div class="logo">
+        <%= link_to root_path, class: "navbar-brand" do %>
+          <%= image_tag "logo.png", class: "logo" %>
+          <h1>Dev<br>ia<br>web</h1>
         <% end %>
-      </ul>
+      </div>
+
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav me-auto">
+          <% if user_signed_in? %>
+            <li class="nav-item active">
+              <%= link_to "Menu", menu_path, class: "btn-navbar" %>
+            </li>
+            <li class="nav-item dropdown">
+              <%= image_tag "profile_picture.png", class: "avatar-bordered dropdown-toggle", id: "navbarDropdown", data: { bs_toggle: "dropdown" }, 'aria-haspopup': true, 'aria-expanded': false %>
+              <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
+                <%= link_to "Menu", menu_path, class: "dropdown-item" %>
+                <%= link_to "Déconnexion", destroy_user_session_path, data: {turbo_method: :delete}, class: "dropdown-item" %>
+              </div>
+            </li>
+          <% else %>
+            <li class="nav-item">
+              <%= link_to "S'inscrire", new_user_registration_path, class: "btn-navbar" %>
+              <%= link_to "Connexion", new_user_session_path, class: "btn-navbar" %>
+            </li>
+          <% end %>
+        </ul>
+      </div>
     </div>
   </div>
-</div>
 HTML
+
+# Menu page
+########################################
+file "app/views/pages/menu.html.erb", <<~HTML
+  <div id="euclidian">
+    <div class="container">
+      <h1>Menu</h1>
+    </div>
+
+    <div class="container">
+      <div class="row">
+
+        <div class="col-md-6 col-lg-4">
+          <div class="cards_item">
+            <%= link_to menu_path do %>
+              <div id="card-1" class="card">
+                <div class="card_image">
+                  <div class="card_content">
+                    <h2 class="card_title">Web</h2>
+                    <p class="card_text">Sites internet, IA, applications, blogs</p>
+                  </div>
+                </div>
+              </div>
+            <% end %>
+          </div>
+        </div>
+
+        <div class="col-md-6 col-lg-4">
+          <div class="cards_item">
+            <%= link_to menu_path do %>
+              <div id="card-2" class="card">
+                <div class="card_image">
+                  <div class="card_content">
+                    <h2 class="card_title">Forum</h2>
+                    <p class="card_text">Discussions, tutoriels, messages</p>
+                  </div>
+                </div>
+              </div>
+            <% end %>
+          </div>
+        </div>
+
+      </div>
+    </div>
+  </div>
+HTML
+
+
 
 # README
 ########################################
@@ -240,6 +288,18 @@ after_bundle do
   file "app/controllers/application_controller.rb", <<~RUBY
     class ApplicationController < ActionController::Base
       before_action :authenticate_user!
+
+      def after_sign_in_path_for(resource)
+        stored_location_for(resource) || menu_path
+      end
+
+      def after_sign_up_path_for(resource)
+        stored_location_for(resource) || menu_path
+      end
+
+      def after_sign_out_path_for(resource)
+        stored_location_for(resource) || menu_path
+      end
     end
   RUBY
 
@@ -263,9 +323,16 @@ after_bundle do
   run "rm app/controllers/pages_controller.rb"
   file "app/controllers/pages_controller.rb", <<~RUBY
     class PagesController < ApplicationController
-      skip_before_action :authenticate_user!, only: [ :home ]
+      skip_before_action :authenticate_user!, only: %i[ home menu puzzle ] # ne pas s'authentifier sur les pages suivantes
+      # before_action :authenticate_user!, except: [:show, :index] # ne pas s'authentifier sur les fonctions suivantes
 
       def home
+      end
+
+      def menu
+      end
+
+      def puzzle
       end
     end
   RUBY
